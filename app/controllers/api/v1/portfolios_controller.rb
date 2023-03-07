7daf2c88a2f1c322519f6a1bfafce6ac906d8922
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-03-07 22:09:27 +0300

    Update to the index portfolio action

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::PortfoliosController < ApplicationController
   def index
-    render json: { message: "portfolio available" }, status: :ok
+    @portfolios = Portfolio.all
+
+    if @portfolios
+      render json: { status: "SUCCESS", message: "portfolio available", data: @portfolios }, status: :ok
+    else
+      render json: @portfolios.errors, status: :bad_request
+    end
   end
 end 
