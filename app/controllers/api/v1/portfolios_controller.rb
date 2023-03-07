fa99bc994327a7b283a318bb97ccb929295a08e5
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-03-07 22:34:13 +0300

    Update included:   - Included the portfolios params   - Created the portfolio create action

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -8,4 +8,20 @@ class Api::V1::PortfoliosController < ApplicationController
       render json: @portfolios.errors, status: :bad_request
     end
   end
+
+  def create
+    portfolio = Portfolio.new(portfolios_params)
+
+    if portfolio.save
+      render json: { status: "SUCCESS", message: "Portfolio was created succesfully", data: portfolio }, status: :created
+    else
+      render json: portfolio.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def portfolios_params
+    params.require(:portfolio).permit(:img, :title, :techStack, :description, :live_link, :howBuiltAbbr, :howBuiltDesc)
+  end
 end 
