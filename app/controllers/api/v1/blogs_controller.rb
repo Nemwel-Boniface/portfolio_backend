f7d15ebba8cd1dfc623464b2b57d6320e8c986b9
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-03-08 08:50:01 +0300

    Update included:   - Included the blogs params   - Created the blog create action

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -7,4 +7,20 @@ class Api::V1::BlogsController < ApplicationController
       render json: @blogs.errors, status: :bad_request
     end
   end
+
+  def create
+    blog = Blog.new(blogs_params)
+
+    if blog.save
+      render json: { status: "SUCCESS", message: "Blog created succesfully", data: blog }, status: :created
+    else
+      render json: blog.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def blogs_params
+    params.require(:blog).permit(:img, :dateWritten, :abbreviation, :abbreviated_name, :article_title, :article_description, :article_link)
+  end
 end
