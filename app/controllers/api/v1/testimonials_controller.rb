f65e5a8a686469add45321e1a981f0fa5e6762fe
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-03-09 21:07:46 +0300

    Update created the index and show actions endpoints for the testimonial resource

--- app/controllers/api/v1/testimonials_controller.rb
+++ app/controllers/api/v1/testimonials_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::TestimonialsController < ApplicationController
+  # List all the testimonials that are available in the database
+  def index
+    testimonials = Testimonial.all
+
+    if testimonials
+      render json: { status: "SUCCESS", message: "Testimonials retrieved succesfully!", data: testimonials }, status: :ok
+    else
+      render json: testimonials.errors, status: :bad_request
+    end
+  end
+
+  # Show details for a specific testimonial
+  def show
+    testimonial = Testimonial.find(params[:id])
+
+    if testimonial
+      render json: { message: "Testimonial was retrieved succesfully", data: testimonial }, status: :ok
+    else
+      render json: { message: "Testimonial could not be found" }, status: :bad_request
+    end
+  end
 end
